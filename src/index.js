87679ac747a9cc1dc3f3dc1e022d0247f9a38d01
iamfiro <appturbo102@gmail.com>
2024-04-07 23:09:33 +0900

    📸 `Actions` 강좌 코드 스냅샷

--- src/index.js
+++ src/index.js
@@ -4,11 +4,23 @@ const add = document.getElementById('add');
 const minus = document.getElementById('minus');
 const number = document.querySelector('span');
 
-const countModifier = (state = 0) => {
-    console.log(state)
-    return state
+const countModifier = (state = 0, action) => {
+    if(action.type === 'ADD') {
+        return state + 1;
+    } else if (action.type === 'MINUS') {
+        return state - 1;
+    } else {
+        return state;
+    }
 }
 
 const countStore = createStore(countModifier);
+
+countStore.dispatch({ type: 'ADD' });
+countStore.dispatch({ type: 'ADD' });
+countStore.dispatch({ type: 'ADD' });
+countStore.dispatch({ type: 'ADD' });
+countStore.dispatch({ type: 'ADD' });
+countStore.dispatch({ type: 'MINUS' });
 
 console.log(countStore.getState())
