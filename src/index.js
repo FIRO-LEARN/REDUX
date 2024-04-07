f894639df4d346fba059ec5e8662778900e58cb1
iamfiro <appturbo102@gmail.com>
2024-04-07 23:03:53 +0900

    📸 `Store and Reducer` 강좌 코드 스냅샷

--- src/index.js
+++ src/index.js
@@ -1,32 +1,14 @@
+import { createStore } from "redux";
+
 const add = document.getElementById('add');
 const minus = document.getElementById('minus');
 const number = document.querySelector('span');
 
-// 카운터의 변수를 정의
-let count = 0;
-
-// 앱 렌더링시 1번만 작동함
-number.innerHTML = count;
-
-const updateText = () => {
-  // count를 number에 표시
-  number.innerHTML = count;
+const countModifier = (state = 0) => {
+    console.log(state)
+    return state
 }
 
-const handleAdd = () => {
-  // count에 1을 더하는 로직을 작성
-  count += 1;
-  // count를 업데이트하는 함수를 호출
-  updateText();
-}
+const countStore = createStore(countModifier);
 
-const handleMinus = () => {
-  // count에 1을 빼는 로직을 작성
-  count -= 1;
-  // count를 업데이트하는 함수를 호출
-  updateText();
-}
-
-// add와 minus 버튼에 클릭 이벤트를 추가
-add.addEventListener('click', handleAdd)
-minus.addEventListener('click', handleMinus)
+console.log(countStore.getState())
